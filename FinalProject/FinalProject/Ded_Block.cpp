973cf8441458cdccac691a00bde2fe6546680b50
Sharon Vilensky <SsharonV@gmail.com>
2018-05-06 17:20:18 +0300

    #8.2 Valid memory management
    the program can terminate without memory leaks due to non-free pointers due the execution

--- FinalProject/FinalProject/Ded_Block.cpp
+++ FinalProject/FinalProject/Ded_Block.cpp
@@ -4,7 +4,14 @@
 Ded_Block::Ded_Block(unsigned int sn, string id, int n_s_f, int s)
 	: b_sn(sn), b_id(id), num_shared_files(n_s_f), size(s)
 {
+	using t_block = map<string, Ded_File*>;
+	using il_blocks = initializer_list<t_block::value_type>;
+	b_files = make_shared<t_block>(il_blocks{});
+}
 
+Ded_Block::~Ded_Block()
+{
+	
 }
 
 void Ded_Block::setBlockSize(unsigned int s)
@@ -14,7 +21,7 @@ void Ded_Block::setBlockSize(unsigned int s)
 
 void Ded_Block::AddFile(Ded_File &f)
 {
-	b_files.push_back(&f);
+	(*b_files)[f.GetSN()] = &f;
 }
 
 string Ded_Block::GetSN()
